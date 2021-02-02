39e5ffff3157154ef1c09b205e347f94c8361621
Karine Riquena Limp <karine_limp@hotmail.com>
2021-02-01 21:15:18 -0300

    Encapsula variaveis de ambiente com dotenv

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import studentRouter from './routes/studentRouter.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Connect to MongoDB by Mongoose
 (async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://karineriquena:123qwe456@bootcamp-fullstack-igti.ecrqm.mongodb.net/grades?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@bootcamp-fullstack-igti.ecrqm.mongodb.net/grades?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -25,4 +28,4 @@ app.use(express.json());
 app.use(cors());
 app.use('/students', studentRouter);
 
-app.listen('3000', () => console.log('API iniciada'));
+app.listen(process.env.PORT, () => console.log('API iniciada'));
